0f47aa15fab1cf5f76404851be56da5bc41ae3a8
Sam O <sam.orend22@gmail.com>
2021-10-18 23:45:26 -0400

    clean

--- authentikGoodSoFar/authentik-src/src/client/authentik_client_driver.ts
+++ authentikGoodSoFar/authentik-src/src/client/authentik_client_driver.ts
@@ -123,7 +123,7 @@ export async function checkProgram(): Promise<void> {
     throw new Error(`Program is not executable`);
   }
 
-  const AUTHENTIK_NFT_ACCOUNT_SEED = 'bbb';
+  const AUTHENTIK_NFT_ACCOUNT_SEED = 'loll';
   authentikPubkey = await PublicKey.createWithSeed(
     payer.publicKey,
     AUTHENTIK_NFT_ACCOUNT_SEED,
